5cb9c93394ea069d9b1a1c7b2519bf8617063f92
Shubham S Padhi <shubhamspadhi@gmail.com>
2026-01-11 16:45:23 +0100

    Started settings section in profile page

--- app/(gradient-bg)/profile/page.tsx
+++ app/(gradient-bg)/profile/page.tsx
@@ -24,6 +24,16 @@ const ProfilePage = () => {
 					name={PROFILE_RECORDS[currUser].name}
 					location={PROFILE_RECORDS[currUser].location}
 				/>
+				<div className="bg-foreground/10 mb-5 h-px w-full" />
+
+				{/*TODO: Extract into different component*/}
+				<div className="flex w-full flex-col gap-2.5">
+					<div>
+						<div className="font-title text-foreground/70 text-sm font-medium uppercase">
+							account settings
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
